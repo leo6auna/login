5dd10dbc693ec07beec9b71370a4f4b68d4b27e1
leo6auna <leonardoemmanuelgauna@gmail.com>
2023-06-20 19:03:49 -0300

    summary

--- api/src/config.js
+++ api/src/config.js
@@ -4,4 +4,4 @@ export const MONGODB_URI =
   process.env.MONGODB_URI || "localhost:27017/merndb";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'some secret key';
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const FRONTEND_URL = "http://localhost:5173";
--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { API_URL } from "../config.js";
 const instance = axios.create({
-    baseURL: API_URL,
+    baseURL: 'https://login-production-cd51.up.railway.app',
     withCredentials: true
 });
 
