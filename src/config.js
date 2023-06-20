e24af478456da8ec5ac2eecb49cafe0315008332
leo6auna <leonardoemmanuelgauna@gmail.com>
2023-06-20 15:50:00 -0300

    comentario

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000
 
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mern";
+  process.env.MONGODB_URI || "mongodb://mongo:zReHqN66ar39xoKF4n6v@containers-us-west-193.railway.app:5732";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'some secret key';
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
