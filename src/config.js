ff1c48bf956a07f2110261feca25dde03408cb92
leo6auna <leonardoemmanuelgauna@gmail.com>
2023-06-20 16:59:37 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000
 
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://mongo:zReHqN66ar39xoKF4n6v@containers-us-west-193.railway.app:5732";
+  process.env.MONGODB_URI || "localhost:27017/merndb";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'some secret key';
 
-export const FRONTEND_URL = "https://login-production-cd51.up.railway.app" || "http://localhost:5173";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
