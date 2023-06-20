c263b5b17f72bd231e37577ce5745d28c4b5a30d
leo6auna <leonardoemmanuelgauna@gmail.com>
2023-06-20 18:13:39 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -7,6 +7,21 @@ import cors from 'cors'
 import { FRONTEND_URL } from './config.js';
 const app = express();
 
+
+app.use((req, res, next)=>{
+	res.header('Access-Control-Allow-Origin', '');
+	res.header('Access-Control-Allow-Credentials', 'true'),
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+);
+	res.header(
+		'Access-Control-Allow-Methods',
+		'GET, POST, OPTIONS,PUT, DELETE'
+);
+	next();
+});
+
 app.use(cors({
     origin: 'https://login-smoky-nine.vercel.app/',
     credentials: true,
