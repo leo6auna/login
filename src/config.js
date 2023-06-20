cabe13735807ed96dbe41074e4e203301a9b8987
leo6auna <leonardoemmanuelgauna@gmail.com>
2023-06-20 00:56:05 -0300

    start command

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000
 
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mern-tasks";
+  process.env.MONGODB_URI || "mongodb://localhost/mern";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'some secret key';
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
