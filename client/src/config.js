9476999cab6ce46a68bfb64ca9b7dc5f5e9bf182
leo6auna <leonardoemmanuelgauna@gmail.com>
2023-06-20 16:39:44 -0300

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = login-production-cd51.up.railway.app|| "http://localhost:3000/api";
+export const API_URL = "https://login-production-cd51.up.railway.app"|| "http://localhost:3000/api";
