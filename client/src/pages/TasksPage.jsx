eb7c9aafc262f8742b5b222adcfde66779956023
leo6auna <leonardoemmanuelgauna@gmail.com>
2023-06-21 00:28:18 -0300

    Update TasksPage.jsx

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -4,10 +4,10 @@ import TaskCard from "../components/TaskCard"
 function TasksPage() {
 
   const { getTasks, tasks } = UseTasks()
-
-  useEffect(()=>{
-    getTasks()
-  },[])
+  
+  useEffect(() => {
+    getTasks();
+  }, []);
 
   return (
   <div className="grid sm:w-full sm:grid-cols-2 lg:grid-cols-3 gap-2">
